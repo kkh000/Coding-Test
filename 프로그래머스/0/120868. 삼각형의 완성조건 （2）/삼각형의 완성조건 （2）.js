40b94fc3bb950b4b4679cfac3f997cc34ad15a24
김규헌 <105029085+kkh000@users.noreply.github.com>
2024-02-04 12:35:08 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -4,3 +4,37 @@ function solution(sides) {
     const subSides = sides[0]-sides[1];
     return Array.from({length : sumSides-subSides-1}).length
 }
+
+
+
+
+
+
+
+/*
+    
+function solution(sides) {
+    return Math.min(...sides)*2-1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function solution(sides) {
+
+    sides.sort((a,b) => b - a);
+
+    return sides[1] + sides[1] -1;
+}
+
+
+*/
