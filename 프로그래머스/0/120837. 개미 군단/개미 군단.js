619956145c5e1fe4b675cb771bb6e54a140494f5
김규헌 <105029085+kkh000@users.noreply.github.com>
2024-02-10 13:57:55 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,7 @@
+function solution(hp) {
+    let general = Math.floor(hp/5);
+    let soldier = Math.floor((hp % 5) / 3);
+    let worker = Math.floor((hp % 5) % 3);
+    
+    return general + soldier + worker
+}
