c45cc9473aa47914bf5ecf445b0c207e1264da71
김규헌 <105029085+kkh000@users.noreply.github.com>
2024-02-12 00:25:41 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -7,3 +7,17 @@ function solution(array) {
     return MaxNumArr.filter(el => el == mode).length > 1 ? -1 : MaxNumArr.indexOf(mode)
     
 }
+
+
+
+
+
+
+
+
+// function solution(array) {
+//     let m = new Map();	//(1)
+//     for (let n of array) m.set(n, (m.get(n) || 0)+1);	//(2)
+//     m = [...m].sort((a,b)=>b[1]-a[1]);	//(3)
+//     return m.length === 1 || m[0][1] > m[1][1] ? m[0][0] : -1;
+// }
