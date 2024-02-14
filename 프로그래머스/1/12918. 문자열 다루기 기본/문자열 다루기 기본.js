6a02da956c75a6b8e9d3db563877de394ede41d3
김규헌 <105029085+kkh000@users.noreply.github.com>
2024-02-14 21:50:28 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,10 +1,4 @@
 function solution(s) {
-    // if( s*0 === 0 && (s.length === 4 || s.length === 6 ) ) {
-    //     return true
-    // } else {
-    //     return false
-    // }
-    
     const ans = [...s].map(el=>(el*0)+1).reduce((a,b)=>a+b);
     return ans === 4 || ans === 6 ? true : false;
 }
