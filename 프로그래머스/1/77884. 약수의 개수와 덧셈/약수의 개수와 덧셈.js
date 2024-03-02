34f8320ea763be12c64dc2bcb5c83995ab437bad
김규헌 <105029085+kkh000@users.noreply.github.com>
2024-03-02 15:00:55 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.95 ms, Memory: 35.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,17 @@
+function solution(left, right) {
+    let ans = [];
+    const arr = Array.from({length: right-left+1},(el,i)=>i+left);
+    const plusMinus =  arr.map(el => {
+        let count = 0;
+        for (let i = 1; i<=el; i++){
+            if(el%i === 0){
+                count ++
+            }
+        }
+        return count
+    }).map(el => el%2 === 0 ? 1 : -1)
+    for(let i = 0; i< arr.length; i++){
+        ans.push(arr[i]*plusMinus[i])
+    }
+    return ans.reduce((a,b)=>(a+b))
+}
