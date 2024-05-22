06216219db6232d277887caca8426a5a0b63b32e
김규헌 <105029085+kkh000@users.noreply.github.com>
2024-05-22 16:14:37 +0900

    [level 3] Title: 단속카메라, Time: 6.80 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,15 +1,14 @@
 function solution(routes) {
     let count = 0;
-    let firstCaremra = -30001;
+    let camera= -30001;
     const startPosition = routes.sort((a, b) => a[1] - b[1])
     
     console.log(startPosition)
 
     for (let route of startPosition) {
-
-        if (firstCaremra  < route[0]) {
+        if (camera < route[0]) {
             count++;
-            firstCaremra = route[1]
+           camera = route[1]
         }
     }
 
